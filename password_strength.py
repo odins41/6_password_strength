a47b65e757203d907af0dd3a858ca7367ac240be
odins41 <odins41@mail.ru>
2016-10-15 23:58:03 +0300

    Update password_strength.py

--- password_strength.py
+++ password_strength.py
@@ -1,8 +1,31 @@
 
+import re
+
+
+def password_blacklist(filepath):
+    black_list = re.findall('\w+', open(filepath).read())
+    return black_list
+
 
 def get_password_strength(password):
-    pass
+    password_strenght = 1
+    if len(password) > 10:
+        password_strenght += 2
+    if re.findall('\d', password):
+        password_strenght += 1
+    if re.findall('[!@#$%^&*()_+№,.]', password):
+        password_strenght += 2
+    if password.lower() != password and password.upper() != password: 
+        password_strenght += 2
+    if password not in (password_blacklist(filepath)):
+        password_strenght += 2 
+    return password_strenght     
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input("Введите путь к файлу черного списка паролей\n")
+    password = input("Введите пароль\n")
+    if password == '':
+        print ('Вы не ввели пароль')
+    else:    
+        print (get_password_strength(password))
